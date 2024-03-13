a020f1f1962e2f45e32bcd2fe2cfa453847f5deb
Jeff Zhang <jefflz@uci.edu>
2024-03-13 16:03:53 -0700

    added fetch to script.js

--- pages/main_page/script.js
+++ pages/main_page/script.js
@@ -20,6 +20,25 @@ usernameDisplay.innerHTML = `
 `;
 sensorDisplay.appendChild(usernameDisplay);
 
+function getData() {
+    const apiUrl = '../../user/1';
+
+    fetch(apiUrl)
+        .then(response => {
+            if (!response.ok) {
+                throw new Error('Network response was not ok');
+            }
+            return response.json();
+        })
+        .then(data => {
+            console.log(data);
+        })
+        .catch(error => {
+            console.error('Error:', error);
+        });
+}
+
+getData();
 
 let itemIdCounter = 0;
 
--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,15 @@
+use agrisensei::start_server;
+
 
 fn main(){
+    // run_tests();
+    start_server("127.0.0.1:5500");
+}
+
+fn run() {
     agrisensei::connect_db();
     let profile  = serde_json::to_string_pretty(&agrisensei::pull_user_profile(1).unwrap()).unwrap();
+
     println!("{}", profile);
 }
 
